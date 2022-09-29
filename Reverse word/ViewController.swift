d9d4124e14b2cce4f10105d82323dbc04527094a
Baykov Andrey <baykov.andrey@gmail.com>
2022-09-29 00:39:42 -0700

    Created UI

---
+++ Reverse word/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reverse word
+//
+//  Created by Andrey Baykov on 9/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
