2ac527ec46253376bd22ba2bfde01a514636921b
Baykov Andrey <baykov.andrey@gmail.com>
2022-09-29 02:26:46 -0700

    Added functionality

--- Reverse word/ViewController.swift
+++ Reverse word/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textOrigin: UITextField!
+    
+    @IBOutlet weak var resultText: UILabel!
+    
+    @IBOutlet weak var reverseButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        reverseButton.isEnabled = false
+        reverseButton.setTitle("Reverse", for: .normal)
+        
     }
-
-
+    
+    @IBAction func clickReverseButton(_ sender: Any) {
+        if (reverseButton.title(for: .normal) == "Reverse"){
+            let arrayOrigin = [String]( textOrigin.text!.components(separatedBy: " "))
+            var arrayOutput = [String]()
+            
+            for word in arrayOrigin{
+                arrayOutput.append(String(word.reversed()))
+            }
+            resultText.text = arrayOutput.joined(separator: " ")
+            reverseButton.setTitle("Clear", for: .normal)
+        }
+        else {
+            textOrigin.text = ""
+            resultText.text = ""
+            reverseButton.setTitle("Reverse", for: .normal)
+            reverseButton.isEnabled = false
+        }
+        
+    }
+    
+    
+    @IBAction func textOriginEditing(_ sender: Any) {
+        if (textOrigin.text == "") {
+            reverseButton.isEnabled = false
+        }
+        else {
+            reverseButton.isEnabled = true
+        }
+    }
 }
 
